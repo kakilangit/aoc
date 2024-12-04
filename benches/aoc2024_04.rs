675c2cc742cb9266aa06d12fd258e5a0a92c3d0a
kakilangit <kakilangit@gmail.com>
2024-12-04 22:33:40 +0100

    Test

--- benches/aoc2024_04.rs
+++ benches/aoc2024_04.rs
@@ -2,7 +2,9 @@ use aoc::aoc2024::day04;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn part_one(c: &mut Criterion) {
-    let monitor = day04::ElfMonitor::new_from_data().unwrap();
+    let mut monitor = day04::ElfMonitor::new_from_data().unwrap();
+    monitor.calculate_vectors();
+
     c.bench_function("count_xmas", |b| b.iter(|| monitor.count_xmas()));
 }
 
