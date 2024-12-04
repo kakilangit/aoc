555b21adee33c0345bfee4d0f2770fa0e30e4d45
kakilangit <kakilangit@gmail.com>
2024-12-04 21:43:31 +0100

    Part 1

---
+++ benches/aoc2024_04.rs
@@ -0,0 +1,10 @@
+use aoc::aoc2024::day04;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn part_one(c: &mut Criterion) {
+    let monitor = day04::ElfMonitor::new_from_data().unwrap();
+    c.bench_function("count_xmas", |b| b.iter(|| monitor.count_xmas()));
+}
+
+criterion_group!(benches, part_one);
+criterion_main!(benches);
