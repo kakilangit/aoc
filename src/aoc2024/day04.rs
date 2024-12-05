d7a9350b0f2b416bb4929fa5aaacc213c648bf47
kakilangit <kakilangit@gmail.com>
2024-12-05 15:23:15 +0100

    Day 4 improvement

--- src/aoc2024/day04.rs
+++ src/aoc2024/day04.rs
@@ -37,17 +37,133 @@ impl Trie {
     }
 }
 
+#[derive(Clone, Copy)]
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+    DiagUpRight,
+    DiagUpLeft,
+    DiagDownRight,
+    DiagDownLeft,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+struct Pos(i32, i32);
+
+impl Pos {
+    // 0.0 is top left
+    fn matrix_path(
+        &self,
+        n: i32,
+        direction: Direction,
+        matrix_size: (i32, i32),
+    ) -> anyhow::Result<Vec<Self>> {
+        if n < 1 {
+            anyhow::bail!("negative is forbidden, use direction to move")
+        }
+
+        let mut path = vec![];
+
+        match direction {
+            Direction::Right => {
+                if self.0 + n > (matrix_size.0 - 1) {
+                    anyhow::bail!("out of bounds")
+                }
+
+                for i in 0..n {
+                    path.push(Pos(self.0 + i + 1, self.1));
+                }
+            }
+            Direction::Left => {
+                if self.0 - n < 0 {
+                    anyhow::bail!("out of bounds")
+                }
+
+                for i in 0..n {
+                    path.push(Pos(self.0 - (i + 1), self.1));
+                }
+            }
+            Direction::Up => {
+                if self.1 - n < 0 {
+                    anyhow::bail!("out of bounds")
+                }
+
+                for i in 0..n {
+                    path.push(Pos(self.0, self.1 - (i + 1)));
+                }
+            }
+            Direction::Down => {
+                if self.1 + n > (matrix_size.1 - 1) {
+                    anyhow::bail!("out of bounds")
+                }
+
+                for i in 0..n {
+                    path.push(Pos(self.0, self.1 + (i + 1)));
+                }
+            }
+            Direction::DiagUpLeft => {
+                for i in 0..n {
+                    let delta = i + 1;
+                    let x = self.0 - delta;
+                    let y = self.1 - delta;
+
+                    if x < 0 || y < 0 {
+                        anyhow::bail!("out of bounds")
+                    }
+
+                    path.push(Pos(x, y));
+                }
+            }
+            Direction::DiagUpRight => {
+                for i in 0..n {
+                    let delta = i + 1;
+                    let x = self.0 + delta;
+                    let y = self.1 - delta;
+
+                    if x > matrix_size.0 - 1 || y < 0 {
+                        anyhow::bail!("out of bounds")
+                    }
+
+                    path.push(Pos(x, y));
+                }
+            }
+            Direction::DiagDownLeft => {
+                for i in 0..n {
+                    let delta = i + 1;
+                    let x = self.0 - delta;
+                    let y = self.1 + delta;
+
+                    if x < 0 || y > matrix_size.1 - 1 {
+                        anyhow::bail!("out of bounds")
+                    }
+
+                    path.push(Pos(x, y));
+                }
+            }
+            Direction::DiagDownRight => {
+                for i in 0..n {
+                    let delta = i + 1;
+                    let x = self.0 + delta;
+                    let y = self.1 + delta;
+
+                    if x > matrix_size.0 - 1 || y > matrix_size.1 - 1 {
+                        anyhow::bail!("out of bounds")
+                    }
+
+                    path.push(Pos(x, y));
+                }
+            }
+        }
+
+        Ok(path)
+    }
+}
+
 #[derive(Default)]
 pub struct ElfMonitor {
     matrix: Vec<Vec<Cow<'static, str>>>,
-    from_right: Vec<Cow<'static, str>>,
-    from_left: Vec<Cow<'static, str>>,
-    from_top: Vec<Cow<'static, str>>,
-    from_bottom: Vec<Cow<'static, str>>,
-    diagonal_top_right: Vec<Cow<'static, str>>,
-    diagonal_top_left: Vec<Cow<'static, str>>,
-    diagonal_bottom_left: Vec<Cow<'static, str>>,
-    diagonal_bottom_right: Vec<Cow<'static, str>>,
 }
 
 impl ElfMonitor {
@@ -65,178 +181,71 @@ impl ElfMonitor {
         })
     }
 
-    pub fn count_xmas_improved(&self) -> i32 {
-        let mut total = 0;
-        let mut pointer = (1, 1);
+    fn find_mas(&self, start: Pos) -> usize {
+        let paths = self.mas_paths_in_all_direction(start);
+        paths
+            .iter()
+            .filter(|p| self.matrix_string(p) == "MAS")
+            .count()
+    }
 
-        let max_down = self.matrix.len() - 2;
-        let max_right = self.matrix[0].len() - 2;
+    fn matrix_string(&self, paths: &[Pos]) -> Cow<'static, str> {
+        paths
+            .iter()
+            .map(|p| {
+                let x = p.0 as usize;
+                let y = p.1 as usize;
 
-        while pointer.0 <= max_down && pointer.1 <= max_right {
-            let c = &self.matrix[pointer.0][pointer.1];
-            if c == "A" {
-                let top_left = &self.matrix[pointer.0 - 1][pointer.1 - 1];
-                let bottom_right = &self.matrix[pointer.0 + 1][pointer.1 + 1];
-                let top_right = &self.matrix[pointer.0 - 1][pointer.1 + 1];
-                let bottom_left = &self.matrix[pointer.0 + 1][pointer.1 - 1];
+                self.matrix[x][y].chars().as_str()
+            })
+            .collect()
+    }
 
-                if Self::valid_cross(top_left, top_right, bottom_left, bottom_right) {
-                    total += 1;
-                }
-            }
-
-            if pointer.1 < max_right {
-                pointer.1 += 1;
+    fn mas_paths_in_all_direction(&self, start: Pos) -> Vec<Vec<Pos>> {
+        let size = (self.matrix[0].len() as i32, self.matrix.len() as i32);
+        [
+            Direction::Right,
+            Direction::Left,
+            Direction::Up,
+            Direction::Down,
+            Direction::DiagUpLeft,
+            Direction::DiagUpRight,
+            Direction::DiagDownLeft,
+            Direction::DiagDownRight,
+        ]
+        .iter()
+        .filter_map(|dir| {
+            if let Ok(path) = start.matrix_path(3, *dir, size) {
+                Some(path)
             } else {
-                pointer.1 = 1;
-                pointer.0 += 1;
+                None
             }
-        }
-
-        total
+        })
+        .collect()
     }
 
-    pub fn calculate_vectors(&mut self) {
-        self.calculate_straights();
-        self.calculate_diagonals();
-    }
-
-    pub fn count_xmas(&self) -> anyhow::Result<i32> {
-        // let mut trie = Trie::new();
-        // trie.insert("XMAS");
-        //
-        let combined: Vec<Cow<'static, str>> = self
-            .from_left
-            .iter()
-            .chain(self.from_right.iter())
-            .chain(self.from_top.iter())
-            .chain(self.from_bottom.iter())
-            .chain(self.diagonal_top_left.iter())
-            .chain(self.diagonal_top_right.iter())
-            .chain(self.diagonal_bottom_left.iter())
-            .chain(self.diagonal_bottom_right.iter())
-            .cloned()
-            .collect();
-
-        Ok(combined
-            .iter()
-            .map(|word| Self::brute_count_xmas(word))
-            .sum::<i32>())
-    }
-
-    fn brute_count_xmas(word: &str) -> i32 {
+    pub fn count_xmas(&self) -> i32 {
         let mut total = 0;
-        let mut chars = word.chars().peekable();
-        while let Some(ch) = chars.next() {
-            if ch == 'X' && chars.peek() == Some(&'M') {
-                chars.next();
-                if chars.peek() == Some(&'A') {
-                    chars.next();
-                    if chars.peek() == Some(&'S') {
-                        chars.next();
-                        total += 1;
-                    }
-                }
+        let mut pos = Pos(0, 0);
+
+        let max_down = self.matrix.len() as i32 - 1;
+        let max_right = self.matrix[0].len() as i32 - 1;
+
+        while pos.0 <= max_down && pos.1 <= max_right {
+            let c = &self.matrix[pos.0 as usize][pos.1 as usize];
+            if c == "X" {
+                total += self.find_mas(pos);
+            }
+
+            if pos.1 < max_right {
+                pos.1 += 1;
+            } else {
+                pos.1 = 0;
+                pos.0 += 1;
             }
         }
 
-        total
-    }
-
-    fn calculate_straights(&mut self) {
-        self.from_left = self
-            .matrix
-            .iter()
-            .map(|row| Cow::from(row.concat()))
-            .collect();
-        self.from_right = self
-            .matrix
-            .iter()
-            .map(|row| Cow::from(row.iter().rev().cloned().collect::<String>()))
-            .collect();
-
-        let num_cols = self.matrix[0].len();
-        self.from_top = (0..num_cols)
-            .map(|col| {
-                Cow::from(
-                    self.matrix
-                        .iter()
-                        .map(|row| &row[col])
-                        .cloned()
-                        .collect::<String>(),
-                )
-            })
-            .collect();
-
-        self.from_bottom = (0..num_cols)
-            .map(|col| {
-                Cow::from(
-                    self.matrix
-                        .iter()
-                        .rev()
-                        .map(|row| &row[col])
-                        .cloned()
-                        .collect::<String>(),
-                )
-            })
-            .collect();
-    }
-
-    fn calculate_diagonals(&mut self) {
-        let n = self.matrix.len();
-
-        let get_diagonal =
-            |start_row: i32, start_col: i32, row_step: i32, col_step: i32, reverse: bool| {
-                let mut diagonal: Vec<Cow<'static, str>> = vec![];
-                let mut row = start_row;
-                let mut col = start_col;
-                let mut current_diagonal = String::new();
-
-                while row >= 0 && row < n as i32 && col >= 0 && col < n as i32 {
-                    current_diagonal.push_str(&self.matrix[row as usize][col as usize]);
-                    row += row_step;
-                    col += col_step;
-                }
-
-                if reverse {
-                    current_diagonal = current_diagonal.chars().rev().collect();
-                }
-
-                diagonal.push(Cow::from(current_diagonal));
-                diagonal
-            };
-
-        for i in 0..n {
-            let diag1 = get_diagonal(n as i32 - 1 - i as i32, 0, -1, 1, false);
-            let diag2 = get_diagonal(n as i32 - 1, i as i32, -1, 1, false);
-            self.diagonal_bottom_left.extend(diag1);
-            self.diagonal_bottom_left.extend(diag2);
-        }
-        self.diagonal_bottom_left.dedup();
-
-        for i in 0..n {
-            let diag1 = get_diagonal(n as i32 - 1 - i as i32, n as i32 - 1, -1, -1, false);
-            let diag2 = get_diagonal(n as i32 - 1, n as i32 - 1 - i as i32, -1, -1, false);
-            self.diagonal_bottom_right.extend(diag1);
-            self.diagonal_bottom_right.extend(diag2);
-        }
-        self.diagonal_bottom_right.dedup();
-
-        for i in 0..n {
-            let diag1 = get_diagonal(n as i32 - 1 - i as i32, 0, -1, 1, true);
-            let diag2 = get_diagonal(n as i32 - 1, i as i32, -1, 1, true);
-            self.diagonal_top_left.extend(diag1);
-            self.diagonal_top_left.extend(diag2);
-        }
-        self.diagonal_top_left.dedup();
-
-        for i in 0..n {
-            let diag1 = get_diagonal(n as i32 - 1 - i as i32, n as i32 - 1, -1, -1, true);
-            let diag2 = get_diagonal(n as i32 - 1, n as i32 - 1 - i as i32, -1, -1, true);
-            self.diagonal_top_right.extend(diag1);
-            self.diagonal_top_right.extend(diag2);
-        }
-        self.diagonal_top_right.dedup();
+        total as i32
     }
 
     pub fn count_crossmas(&self) -> i32 {
@@ -291,14 +300,13 @@ impl ElfMonitor {
 
 #[cfg(test)]
 mod test {
-    use super::ElfMonitor;
+    use super::*;
 
     #[test]
     fn test_4_1() {
-        let mut monitor = ElfMonitor::new_from_data().unwrap();
-        monitor.calculate_vectors();
+        let monitor = ElfMonitor::new_from_data().unwrap();
 
-        let total = monitor.count_xmas().unwrap();
+        let total = monitor.count_xmas();
         assert_eq!(2562, total);
     }
 
@@ -307,6 +315,68 @@ mod test {
         let monitor = ElfMonitor::new_from_data().unwrap();
 
         let total = monitor.count_crossmas();
-        assert_eq!(9, total);
+        assert_eq!(1902, total);
+    }
+
+    #[test]
+    fn test_path() {
+        let pos = Pos(5, 5);
+        let size = (10, 10);
+
+        struct Tc {
+            n: i32,
+            dir: Direction,
+            expected: Vec<Pos>,
+            msg: &'static str,
+        }
+        let tcs = [
+            Tc {
+                n: 1,
+                dir: Direction::Right,
+                expected: vec![Pos(6, 5)],
+                msg: "right",
+            },
+            Tc {
+                n: 1,
+                dir: Direction::Up,
+                expected: vec![Pos(5, 4)],
+                msg: "top",
+            },
+            Tc {
+                n: 4,
+                dir: Direction::Down,
+                expected: vec![Pos(5, 6), Pos(5, 7), Pos(5, 8), Pos(5, 9)],
+                msg: "down",
+            },
+            Tc {
+                n: 2,
+                dir: Direction::DiagUpLeft,
+                expected: vec![Pos(4, 4), Pos(3, 3)],
+                msg: "diagonal top left",
+            },
+            Tc {
+                n: 2,
+                dir: Direction::DiagUpRight,
+                expected: vec![Pos(6, 4), Pos(7, 3)],
+                msg: "diagonal top right",
+            },
+            Tc {
+                n: 2,
+                dir: Direction::DiagDownLeft,
+                expected: vec![Pos(4, 6), Pos(3, 7)],
+                msg: "diagonal bottom left",
+            },
+            Tc {
+                n: 2,
+                dir: Direction::DiagDownRight,
+                expected: vec![Pos(6, 6), Pos(7, 7)],
+                msg: "diagonal bottom right",
+            },
+        ];
+
+        for t in tcs {
+            let actual = pos.matrix_path(t.n, t.dir, size).unwrap();
+            assert_eq!(t.expected, actual, "{}", t.msg);
+        }
     }
 }
